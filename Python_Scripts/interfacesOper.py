73773239b07f4c784a57b24552175080fe7cfdd8
Kingsley Idoko <idoko.omokhodion@gmail.com>
2024-03-23 11:16:05 +0100

    Snapshot of server

--- Python_Scripts/interfacesOper.py
+++ Python_Scripts/interfacesOper.py
@@ -5,8 +5,7 @@ from rich import print
 import os
 from xml.dom import minidom
 from lxml import etree
-from utiliites_scripts.security_zones import select_zone, update_zone, zone_interface_names,find_available_interfaces
-from utiliites_scripts.interfaces import get_interfaces
+from utiliites_scripts.interfaces import select_interface,config_interface
 from utiliites_scripts.commit import run_pyez_tasks
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -20,13 +19,12 @@ class InterfaceManager:
         while True:
             print("\nSpecify Operation.....")
             print("1. Get Intefaces")
-            print("2. Delete Interfaces")
+            print("2. Create Interfaces")
             print("3. Update Interfaces")
             print("4. Delete Interfaces")
             operation = input("Enter your choice (1-4): ")
             if operation == "1":
-                response = self.get_interfaces()
-                return response
+                response = self.get_interfaces(interactive=True)
             elif operation == "2":
                 response = self.create_interfaces()
                 return response
@@ -40,11 +38,36 @@ class InterfaceManager:
                 print("Invalid choice. Please specify a valid operation.")
                 continue
 
-    def get_interfaces(self):
-        pass
-    
+    def get_interfaces(self, interactive=False, retries=3):
+        attempt = 0
+        while attempt < retries:
+            try:
+                response = self.nr.run(task=pyez_get_config, database=self.database)
+                if response:
+                    all_results = []  
+                    for interface in response:
+                        result = response[interface].result['configuration']['interfaces']['interface']
+                        all_results.append(result)  
+                    if interactive:
+                        for result in all_results:
+                            print(result)  
+                    return all_results  
+                else:
+                    print("No response received, trying again...")
+            except Exception as e:
+                print(f"An error has occurred: {e}")
+                print("Checking connectivity to the device, trying again...")
+            attempt += 1
+        print("Failed to retrieve interfaces after several attempts.")
+        return None
+
+
     def create_interfaces(self):
-        pass
+        interfaces = self.get_interfaces()
+        filtered_interface = select_interface(interfaces)
+        int_params = ['description', 'disable', 'encapsulation', 'ether-options', 'gigether-options', 
+        'hold-time', 'link-mode', 'mac', 'mtu', 'speed', 'unit', 'vlan-tagging']  
+        config_interface(int_params, filtered_interface)
 
     def update_interfaces(self):
         pass
--- Python_Scripts/utiliites_scripts/interfaces.py
+++ Python_Scripts/utiliites_scripts/interfaces.py
@@ -1,21 +1,99 @@
+import re
 def get_interfaces(device_interfaces):
     interface_names = []
-
     for interface in device_interfaces['interface']:
-        # Exclude interfaces starting with 'fab' and 'fxp'
         if interface['name'].startswith('fab') or interface['name'].startswith('fxp'):
             continue
-
-        # Check if 'gigether-options' is present and prefer 'parent' if available
         gigether_options = interface.get('gigether-options')
         if gigether_options:
             parent = gigether_options.get('redundant-parent', {}).get('parent')
             if parent:
                 interface_names.append(parent)
-                continue  # Move to the next interface after adding the parent
-        
-        # Add the interface name if not processed above
+                continue  
         interface_names.append(interface['name'])
-
-    # Remove duplicates to ensure the interfaces returned are unique
     return list(set(interface_names))
+
+def is_valid_string(input_string):
+    pattern = r'^[a-zA-Z0-9_ ]+$'
+    if re.match(pattern, input_string):
+        words = input_string.split()
+        return len(words) <= 10
+    else:
+        return False
+    
+def select_interface(nested_interfaces):
+    if not nested_interfaces or not isinstance(nested_interfaces[0], list):
+        print("Invalid interface data provided.")
+        return
+    interfaces = nested_interfaces[0]  
+    while True:  
+        print("Please choose an interface for configuration by entering its number:\n")
+        for index, interface in enumerate(interfaces, start=1):
+            print(f"{index}. {interface['name']}")
+        
+        try:
+            choice = int(input("\nEnter choice: ")) - 1 
+            if choice < 0 or choice >= len(interfaces):
+                print("Invalid choice. Please enter a number listed above.\n")
+                continue  
+            selected_interface = interfaces[choice]
+            print(f"\nYou selected: {selected_interface['name']}")
+            return selected_interface 
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+
+
+
+def config_interface(int_params, filtered_interface):
+    print(f"Please what would you like to configure for interface {filtered_interface['name']}:\n")
+    for index, name in enumerate(int_params, start=1):
+        print(f"{index}. {name}")
+
+    while True:  
+        try:
+            choice = int(input("\nEnter choice: ")) - 1  
+            if choice < 0 or choice >= len(int_params):
+                print("Invalid choice. Please enter a number listed above.\n")
+                continue  
+            selected_param = int_params[choice]
+            print(f"\nYou selected: {selected_param}")
+            if choice == 0:
+                description = input("Enter Description: ")
+                print(description)  
+            elif choice == 1:
+                response = interface_status(filtered_interface['name'])
+            elif choice == 2:
+                response = ether_options(filtered_interface['name'])
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+
+
+def create_interface(interface_name):
+    ['ethernet-switching','inet','inet6','iso','mpls']               
+    payload = f"""
+    <configuration>
+            <interfaces>
+                <interface operation="create">
+                    <name>{interface_name}</name>
+                    <unit>
+                        <name>{config_unit()}</name>
+                    </unit>
+                </interface>
+            </interfaces>
+    </configuration>"""
+    return payload 
+
+def ether_options(interface_name):
+    pass
+def gigabit_options(interface_name):
+    pass
+def config_mac(interface_name):
+    pass
+def config_speed(interface_name):
+    pass
+def config_unit(interface_name):
+    pass
+def config_linkmode(interface_name):
+    pass
+def config_vlan_tagging(interface_name):
+    pass
