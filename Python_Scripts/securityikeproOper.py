1757618ee43ae0b7b5ebcfa235522e8175ab7a03
kingsleyidoko <idoko.omokhodion@gmail.com>
2024-04-05 08:51:02 +0200

    worked on ikeproposal update function

---
+++ Python_Scripts/securityikeproOper.py
@@ -0,0 +1,103 @@
+
+from nornir_pyez.plugins.tasks import pyez_get_config,pyez_config, pyez_commit, pyez_diff
+from nornir import InitNornir
+from rich import print
+import os
+from utiliites_scripts.commit import run_pyez_tasks
+from utiliites_scripts.proposals import (gen_ikeprop_config, extract_and_update_proposal, 
+                                         gen_ikeproposal_xml)
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+class IkeProposalManager:
+    database = 'committed'
+    def __init__(self, config_file="config.yml"):
+        self.nr = InitNornir(config_file=config_file)
+
+    def ike_operations(self):
+        while True:
+            print("\nSpecify Operation.....")
+            print("1. Get Ike Proposals")
+            print("2. Create Ike Proposal")
+            print("3. Update Ike Proposal")
+            print("4. Delete Ike Proposal")
+            operation = input("Enter your choice (1-4): ")
+            if operation == "1":
+                response = self.get_proposals(interactive=True)
+            elif operation == "2":
+                response = self.create_proposal()
+                return response
+            elif operation == "3":
+                response = self.update_proposal()
+                return response
+            elif operation == "4":
+                response =  self.delete_proposal()
+                return response
+            else:
+                print("Invalid choice. Please specify a valid operation.")
+                continue
+
+    def get_proposals(self, interactive=False, get_raw_data=False, retries=3):
+        attempt = 0
+        while attempt < retries:
+            try:
+                response = self.nr.run(task=pyez_get_config, database=self.database)
+                if response:
+                    for _, result in response.items():
+                        ike_config = result.result['configuration']['security'].get('ike')
+                        if ike_config:
+                            proposals = ike_config.get('proposal',[])
+                            proposals = [proposals] if isinstance(proposals, dict) else proposals
+                            ike_proposal_names = [proposal['name'] for proposal in proposals if 'name' in proposal]
+                            if not isinstance(ike_proposal_names, list):  
+                                ike_proposal_names = [ike_proposal_names]
+                        else:
+                            print("No Security ike implementation exist on the device")
+                            break
+                    if interactive:
+                        print(ike_proposal_names)
+                    if get_raw_data:
+                        return ike_config, ike_proposal_names if ike_config else None
+                    return ike_proposal_names if ike_proposal_names else None
+            except Exception as e:
+                print(f"An error has occurred: {e}. Checking connectivity to the device, trying again...")
+            attempt += 1
+        print("Failed to retrieve proposals after several attempts.")
+        return None
+
+    
+    def create_proposal(self):
+        old_proposals = self.get_proposals()
+        if not old_proposals:
+            print("No existing IKE Proposal found on the device")
+        payload = gen_ikeprop_config(old_proposals)
+        return payload
+
+    def update_proposal(self):
+        ike_configs, ike_proposal_names = self.get_proposals(get_raw_data=True)
+        if ike_configs:
+            updated_proposal = extract_and_update_proposal(ike_configs)
+            payload = gen_ikeproposal_xml(updated_proposal, ike_proposal_names)
+            return payload
+        else:
+            print("No existing IKE Proposal exist on the device")
+
+    
+    def delete_proposal(self):
+        # old_proposals = self.get_proposals(get_raw_data=True)
+        # print(old_proposals)
+        pass
+
+    def push_config(self):
+        xml_data = self.ike_operations()
+        print(xml_data)
+        if not xml_data:
+            return 
+        elif isinstance(xml_data, list):
+            for xml in xml_data:
+                run_pyez_tasks(self, xml, 'xml') 
+            return None
+        else:
+             run_pyez_tasks(self, xml_data, 'xml') 
+
+config = IkeProposalManager()
+response = config.push_config()
--- Python_Scripts/utiliites_scripts/proposals.py
+++ Python_Scripts/utiliites_scripts/proposals.py
@@ -31,7 +31,7 @@ def gen_ikeprop_config(old_proposal, encrypt=encrypt,dh_group=dh_group, auth_met
         auth_algorithm = auth_algo[get_valid_integer("Enter choice: ") - 1]
         ike_lifetime = get_valid_integer("Enter Ike Security Association lifetime (180..86400 seconds): ")
         if old_proposal:
-                last_old_proposal = old_proposal[-1]  # Reference the last proposal in the list
+                last_old_proposal = old_proposal[-1]  
                 ike_opening_tag = "<ike>"
                 proposal_attributes = f'insert="after" key="[ name=\'{last_old_proposal}\' ]" operation="create"'
         else:
@@ -59,5 +59,61 @@ def gen_ikeprop_config(old_proposal, encrypt=encrypt,dh_group=dh_group, auth_met
 
 
 
+def extract_and_update_proposal(ike_config):
+    # Normalize proposals to always be a list
+    proposals = ike_config['proposal']
+    if isinstance(proposals, dict):
+        proposals = [proposals]
+    
+    # Extract proposal names and display them for user selection
+    proposal_names = [proposal['name'] for proposal in proposals]
+    for i, name in enumerate(proposal_names, start=1):
+        print(f"{i}. {name}")
+    
+    selected_index = get_valid_integer("Select a proposal to update (by number): ") - 1
+    selected_proposal = proposals[selected_index]
+    
+    print("Selected Proposal:", selected_proposal['name'])
+    # Display keys for the selected proposal
+    proposal_keys = list(selected_proposal.keys())
+    for i, key in enumerate(proposal_keys, start=1):
+        print(f"{i}. {key}")
+    
+    key_index = get_valid_integer("Select a key to update (by number): ") - 1
+    selected_key = proposal_keys[key_index]
+    
+    # Get the new value for the selected key
+    new_value = get_valid_string(f"Enter new value for {selected_key}: ")
+    
+    # Update the selected key with the new value
+    selected_proposal[selected_key] = new_value
+    return selected_proposal
 
 
+
+def gen_ikeproposal_xml(updated_proposal, old_proposal):
+    if old_proposal:
+                last_old_proposal = old_proposal[-1]  
+                ike_opening_tag = "<ike>"
+                proposal_attributes = f'insert="after" key="[ name=\'{last_old_proposal}\' ]" operation="create"'
+    else:
+        ike_opening_tag = '<ike operation="create">'
+        proposal_attributes = ""
+    ike_proposal_xml = f"""
+        <configuration>
+            <security>
+                {ike_opening_tag}
+                    <proposal {proposal_attributes}>
+                        <name>{updated_proposal['name']}</name>
+                        <description>{updated_proposal['description']}</description>
+                        <authentication-method>{updated_proposal['authentication-method']}</authentication-method>
+                        <dh-group>{updated_proposal['dh-group']}</dh-group>
+                        <authentication-algorithm>{updated_proposal['authentication-algorithm']}</authentication-algorithm>
+                        <encryption-algorithm>{updated_proposal['encryption-algorithm']}</encryption-algorithm>
+                        <lifetime-seconds>{updated_proposal['lifetime-seconds']}</lifetime-seconds>
+                    </proposal>
+                </ike>
+            </security>
+        </configuration>""".strip()
+    print("Generated IKE Proposal Configuration:")
+    return ike_proposal_xml
