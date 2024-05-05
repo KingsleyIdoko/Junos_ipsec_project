ee83a47f6a1df961306a8545cd072b0e18104222
kingsleyidoko <idoko.omokhodion@gmail.com>
2024-05-05 20:01:43 +0200

    updating security policy update function

--- Python_Scripts/utiliites_scripts/sec_policies.py
+++ Python_Scripts/utiliites_scripts/sec_policies.py
@@ -5,7 +5,7 @@ from sec_ipsec_vpn import IpsecVpnManager
 zone_manager = SecurityZoneManager()
 address_manager = AddressBookManager()
 vpn_manager = IpsecVpnManager()
-import itertools, re
+import itertools, re, pprint
 
 app = ["any","junos-aol","junos-bgp","junos-biff","junos-bootpc","junos-bootps","junos-defaults","junos-dhcp-client","junos-dhcp-relay","junos-dhcp-server",    
   "junos-discard","junos-dns-tcp","junos-dns-udp","junos-echo","junos-finger","junos-ftp","junos-ftp-data"]
@@ -19,12 +19,16 @@ def generate_zone_directions(zones):
         directions.append(f"zone_{second_zone}_to_{first_zone}")
     return directions
 
+
 def confirm_policy_name(zone_dir):
     pattern = r'^(yes|no)$'
     while True:
-        choice = input(f"Automatically setting the policy_name to: {zone_dir}. Confirm? (yes/no) ")
+        choice = input(f"Automatically setting the policy name to: {zone_dir}. Confirm? (yes/no) ")
         if re.match(pattern, choice, re.IGNORECASE):
-            return choice.lower() == 'yes'
+            if choice.lower() == 'yes':
+                return zone_dir
+            else:
+                zone_dir = get_valid_name("Please enter new policy name: ")
 
 def get_subnet_names_by_zone(addresses, zone_name):
     subnet_names = []
@@ -92,7 +96,6 @@ def gen_sec_policies_config(**kwargs):
     zone_direction = []
     if raw_data:
         policy_data, zone_direction = extract_policy_names(raw_data)
-        old_policy_names = [policy for zone in policy_data.values() for policy in zone['policies']]
         list_zones = list(policy_data.keys())
         if len(list_zones) != expected_combinations:
             list_zones = list(set(list_zones).union(generate_zone_directions(zones)))
@@ -104,10 +107,8 @@ def gen_sec_policies_config(**kwargs):
     print(f"Selected from_zone: {from_zone}, to_zone: {to_zone}")
     if policy_data:
         zone_dir = update_zone_dir(policy_data, zone_dir )
-        reverse_policy = get_reverse_policy(policy_data, from_zone, to_zone)
     else:
         zone_dir = zone_dir 
-        reverse_policy = []
     zone_dir  = confirm_policy_name(zone_dir)
     desc = get_valid_string("Enter policy description: ")
     addresses = address_manager.get_address_book(get_addresses=True)
@@ -122,10 +123,9 @@ def gen_sec_policies_config(**kwargs):
     print(policy_xml)
     return policy_xml
 
-
 def update_zone_dir(policy_data, zone_dir_choice):
     if any(zone_dir_choice == policy_key for policy_key in policy_data):
-        new_zone_dir = input(f"{zone_dir_choice} is already in use. Please enter a new zone direction name: ")
+        new_zone_dir = get_valid_name(f"{zone_dir_choice} is already in use. Please enter a new zone direction name: ")
         return new_zone_dir
     return zone_dir_choice
 
@@ -137,12 +137,9 @@ def get_reverse_policy(policy_data, from_zone, to_zone):
 
 def get_tunnel_config(from_zone, to_zone, get_vpn, policy_data, zone_direction):
     selected_vpn = pair_policy = ""
-    if not policy_data:
-        print("No policy data available.")
-        return ""
     choice = get_valid_selection("Enter valid Permit action", ['tunnel', 'None'])
     if choice == "tunnel":
-        vpn_options = get_vpn()  
+        vpn_options = get_vpn 
         if vpn_options:
             selected = get_valid_selection("Enter IPsec VPN: ", vpn_options)
             reverse_policy = get_reverse_policy(policy_data, from_zone, to_zone)
@@ -183,3 +180,104 @@ def build_policy_xml(from_zone, to_zone, zone_dir, desc, src_address, dst_addres
             </policies>
         </security>
     </configuration>""".strip()
+
+
+def gen_update_config(raw_data):
+    zones = zone_manager.get_security_zone(get_zone_name=True)
+    list_zones = generate_zone_directions(zones)
+    zone_dir = get_valid_selection("Please select traffic zone: ", list_zones)
+    from_zone, to_zone = zone_dir.split('_')[1], zone_dir.split('_')[-1]
+    policy_data, zone_direction = extract_policy_names(raw_data)
+    selected_policies = selected_zone(raw_data, from_zone, to_zone)
+    list_policy_names = [policy['name'] for policy in selected_policies]
+    edit_policy_name = get_valid_selection("Please select a policy to view details: ", list_policy_names)
+    selected_policy = next((policy for policy in selected_policies if policy['name'] == edit_policy_name), None)
+    choice = display_and_select_policy_details(selected_policy, from_zone, to_zone, policy_data)
+    print(choice)
+    if not selected_policy:
+        print("Selected policy not found.")
+        return {}
+    return selected_policy
+
+
+def selected_zone(raw_data, from_zone, to_zone):
+    selected_policies = []  
+    for entry in raw_data:
+        if entry['from-zone-name'] == from_zone and entry['to-zone-name'] == to_zone:
+            if isinstance(entry['policy'], list):
+                selected_policies = entry['policy']
+            else:
+                selected_policies = [entry['policy']]
+            break 
+    return selected_policies
+
+def display_and_select_policy_details(selected_policy, from_zone, to_zone, policy_data):
+    policy = selected_policy
+    while True:
+        keys = list(policy.keys())
+        print("\nPlease select a detail to view or modify:")
+        for index, key in enumerate(keys):
+            value = policy[key]
+            print(f"{index + 1}. {key}: {'{...}' if isinstance(value, dict) else value}")
+        
+        choice_input = input("Enter the number of the detail (0 to exit or 'back' to go up): ")
+        if choice_input.isdigit():
+            choice = int(choice_input) - 1
+        else:
+            if choice_input.lower() == 'back' or choice_input == '0':
+                print("Exiting or going back.")
+                break
+            else:
+                print("Invalid input. Please enter a number or 'back' to go up.")
+                continue
+
+        if 0 <= choice < len(keys):
+            selected_key = keys[choice]
+            selected_value = policy[selected_key]
+            if isinstance(selected_value, dict):
+                display_and_select_policy_details(selected_value, from_zone, to_zone, policy_data)
+            else:
+                if confirm_edit(f"Do you want to edit '{selected_key}'? (yes/no): "):
+                    new_value = get_new_value(selected_key, from_zone, to_zone)
+                    policy[selected_key] = new_value
+                    return policy
+        else:
+            print("Invalid selection. Please try again.")
+
+def confirm_edit(prompt):
+    response = input(prompt)
+    return response.lower() == 'yes'
+
+def get_new_value(key, from_zone, to_zone, policy_data):
+    if key in ['source-address', 'destination-address']:
+        addresses = address_manager.get_address_book(get_addresses=True)
+        address_options = get_subnet_names_by_zone(addresses, from_zone if key == 'source-address' else to_zone)
+        return get_valid_selection(f"Select new {key}: ", address_options)
+    elif key == 'application':
+        return get_valid_selection("Enter new application to permit: ",app)
+    elif key == 'description':
+        return get_valid_string("Enter new description: ")
+    elif key == "ipsec-vpn":
+        get_vpn = vpn_manager.get_ipsec_vpn(get_vpn_name=True)
+        return get_valid_selection("Select new Ipsec VPN tunnel: ",get_vpn)
+    # elif key == "pair-policy":
+    #         reverse_policy = get_reverse_policy(policy_data, from_zone, to_zone)
+    #         if reverse_policy:
+    #             reverse_policy_choice = get_valid_selection("Select Pair: ", reverse_policy)
+    #             return get_valid_selection("Select new Pair-Policy:", )
+    else:
+        return get_valid_name(f"Enter new value for '{key}': ")
+    
+def confirm_deeper_navigation():
+    response = input("Do you want to explore this section further? (yes/no): ")
+    return response.lower() == 'yes'
+
+
+def confirm_action(prompt):
+    import re
+    while True:
+        user_input = input(prompt)
+        if re.match(r'^(yes|no)$', user_input, re.IGNORECASE):
+            return user_input.lower() == 'yes'
+        else:
+            print("Please enter 'yes' or 'no'.")
